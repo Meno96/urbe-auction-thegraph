e6e6133963f795b6c0d60adf7b422744539d2ff7
Daniele <danielemeno96@gmail.com>
2023-02-15 14:22:25 +0100

    connect subgraph

---
+++ src/urb-e-auction.ts
@@ -0,0 +1,110 @@
+import {
+  AuctionEnded as AuctionEndedEvent,
+  HighestBidIncreased as HighestBidIncreasedEvent,
+  ItemBought as ItemBoughtEvent,
+  ItemCanceled as ItemCanceledEvent,
+  ItemListed as ItemListedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent
+} from "../generated/UrbEAuction/UrbEAuction"
+import {
+  AuctionEnded,
+  HighestBidIncreased,
+  ItemBought,
+  ItemCanceled,
+  ItemListed,
+  OwnershipTransferred
+} from "../generated/schema"
+
+export function handleAuctionEnded(event: AuctionEndedEvent): void {
+  let entity = new AuctionEnded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.winner = event.params.winner
+  entity.nftAddress = event.params.nftAddress
+  entity.tokenId = event.params.tokenId
+  entity.price = event.params.price
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleHighestBidIncreased(
+  event: HighestBidIncreasedEvent
+): void {
+  let entity = new HighestBidIncreased(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.bidder = event.params.bidder
+  entity.price = event.params.price
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleItemBought(event: ItemBoughtEvent): void {
+  let entity = new ItemBought(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.buyer = event.params.buyer
+  entity.nftAddress = event.params.nftAddress
+  entity.tokenId = event.params.tokenId
+  entity.price = event.params.price
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleItemCanceled(event: ItemCanceledEvent): void {
+  let entity = new ItemCanceled(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.nftAddress = event.params.nftAddress
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleItemListed(event: ItemListedEvent): void {
+  let entity = new ItemListed(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.seller = event.params.seller
+  entity.nftAddress = event.params.nftAddress
+  entity.tokenId = event.params.tokenId
+  entity.price = event.params.price
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ tests/urb-e-auction-utils.ts
@@ -0,0 +1,171 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  AuctionEnded,
+  HighestBidIncreased,
+  ItemBought,
+  ItemCanceled,
+  ItemListed,
+  OwnershipTransferred
+} from "../generated/UrbEAuction/UrbEAuction"
+
+export function createAuctionEndedEvent(
+  winner: Address,
+  nftAddress: Address,
+  tokenId: BigInt,
+  price: BigInt
+): AuctionEnded {
+  let auctionEndedEvent = changetype<AuctionEnded>(newMockEvent())
+
+  auctionEndedEvent.parameters = new Array()
+
+  auctionEndedEvent.parameters.push(
+    new ethereum.EventParam("winner", ethereum.Value.fromAddress(winner))
+  )
+  auctionEndedEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  auctionEndedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  auctionEndedEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+
+  return auctionEndedEvent
+}
+
+export function createHighestBidIncreasedEvent(
+  bidder: Address,
+  price: BigInt
+): HighestBidIncreased {
+  let highestBidIncreasedEvent = changetype<HighestBidIncreased>(newMockEvent())
+
+  highestBidIncreasedEvent.parameters = new Array()
+
+  highestBidIncreasedEvent.parameters.push(
+    new ethereum.EventParam("bidder", ethereum.Value.fromAddress(bidder))
+  )
+  highestBidIncreasedEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+
+  return highestBidIncreasedEvent
+}
+
+export function createItemBoughtEvent(
+  buyer: Address,
+  nftAddress: Address,
+  tokenId: BigInt,
+  price: BigInt
+): ItemBought {
+  let itemBoughtEvent = changetype<ItemBought>(newMockEvent())
+
+  itemBoughtEvent.parameters = new Array()
+
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam("buyer", ethereum.Value.fromAddress(buyer))
+  )
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+
+  return itemBoughtEvent
+}
+
+export function createItemCanceledEvent(
+  nftAddress: Address,
+  tokenId: BigInt
+): ItemCanceled {
+  let itemCanceledEvent = changetype<ItemCanceled>(newMockEvent())
+
+  itemCanceledEvent.parameters = new Array()
+
+  itemCanceledEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  itemCanceledEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return itemCanceledEvent
+}
+
+export function createItemListedEvent(
+  seller: Address,
+  nftAddress: Address,
+  tokenId: BigInt,
+  price: BigInt
+): ItemListed {
+  let itemListedEvent = changetype<ItemListed>(newMockEvent())
+
+  itemListedEvent.parameters = new Array()
+
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+
+  return itemListedEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
+    newMockEvent()
+  )
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
