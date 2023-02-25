afd597133dcb1f56d6a3c00bb20aa34da7049063
Daniele <danielemeno96@gmail.com>
2023-02-25 18:15:08 +0100

    change contract address

---
+++ tests/urb-e-auction-utils.ts
@@ -0,0 +1,146 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  AuctionEnded,
+  HighestBidIncreased,
+  ItemCanceled,
+  ItemListed
+} from "../generated/UrbEAuction/UrbEAuction"
+
+export function createAuctionEndedEvent(
+  winner: Address,
+  nftAddress: Address,
+  tokenId: BigInt,
+  price: BigInt
+): AuctionEnded {
+  let auctionEndedEvent = changetype<AuctionEnded>(newMockEvent())
+
+  auctionEndedEvent.parameters = new Array()
+
+  auctionEndedEvent.parameters.push(
+    new ethereum.EventParam("winner", ethereum.Value.fromAddress(winner))
+  )
+  auctionEndedEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  auctionEndedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  auctionEndedEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+
+  return auctionEndedEvent
+}
+
+export function createHighestBidIncreasedEvent(
+  bidder: Address,
+  nftAddress: Address,
+  tokenId: BigInt,
+  price: BigInt
+): HighestBidIncreased {
+  let highestBidIncreasedEvent = changetype<HighestBidIncreased>(newMockEvent())
+
+  highestBidIncreasedEvent.parameters = new Array()
+
+  highestBidIncreasedEvent.parameters.push(
+    new ethereum.EventParam("bidder", ethereum.Value.fromAddress(bidder))
+  )
+  highestBidIncreasedEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  highestBidIncreasedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  highestBidIncreasedEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+
+  return highestBidIncreasedEvent
+}
+
+export function createItemCanceledEvent(
+  seller: Address,
+  nftAddress: Address,
+  tokenId: BigInt
+): ItemCanceled {
+  let itemCanceledEvent = changetype<ItemCanceled>(newMockEvent())
+
+  itemCanceledEvent.parameters = new Array()
+
+  itemCanceledEvent.parameters.push(
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
+  )
+  itemCanceledEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  itemCanceledEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return itemCanceledEvent
+}
+
+export function createItemListedEvent(
+  seller: Address,
+  nftAddress: Address,
+  tokenId: BigInt,
+  price: BigInt,
+  endTime: BigInt,
+  startTime: BigInt
+): ItemListed {
+  let itemListedEvent = changetype<ItemListed>(newMockEvent())
+
+  itemListedEvent.parameters = new Array()
+
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam(
+      "endTime",
+      ethereum.Value.fromUnsignedBigInt(endTime)
+    )
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam(
+      "startTime",
+      ethereum.Value.fromUnsignedBigInt(startTime)
+    )
+  )
+
+  return itemListedEvent
+}
---
+++ tests/urb-e-auction.test.ts
@@ -0,0 +1,76 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address, BigInt } from "@graphprotocol/graph-ts"
+import { AuctionEnded } from "../generated/schema"
+import { AuctionEnded as AuctionEndedEvent } from "../generated/UrbEAuction/UrbEAuction"
+import { handleAuctionEnded } from "../src/urb-e-auction"
+import { createAuctionEndedEvent } from "./urb-e-auction-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let winner = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let nftAddress = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let tokenId = BigInt.fromI32(234)
+    let price = BigInt.fromI32(234)
+    let newAuctionEndedEvent = createAuctionEndedEvent(
+      winner,
+      nftAddress,
+      tokenId,
+      price
+    )
+    handleAuctionEnded(newAuctionEndedEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("AuctionEnded created and stored", () => {
+    assert.entityCount("AuctionEnded", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "AuctionEnded",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "winner",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "AuctionEnded",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "nftAddress",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "AuctionEnded",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "tokenId",
+      "234"
+    )
+    assert.fieldEquals(
+      "AuctionEnded",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "price",
+      "234"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
